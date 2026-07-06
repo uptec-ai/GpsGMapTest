bfc2ff19abdf324c02641b7bab86cb82ba9a937a
kong_jy <uptec.ai@gmail.com>
2026-07-06 16:24:28 +0900

    chore: delete net48

---
+++ GpsMapTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// 어셈블리 정보
+[assembly: AssemblyTitle("GpsMapTester")]
+[assembly: AssemblyDescription("BU-353N GPS + GMap.NET 실시간 위치 표출")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GpsMapTester")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// WPF 테마/리소스 위치
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,          // 테마별 리소스 사전 위치
+    ResourceDictionaryLocation.SourceAssembly // 일반(테마 비종속) 리소스 사전 위치
+)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ko-KR")]
